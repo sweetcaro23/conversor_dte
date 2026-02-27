e1d8392852dd3db8de288c19aa2585e1d0693262
sweetcaro23 <carolinafloresm.analista@gmail.com>
2026-02-27 16:10:34 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\DteController;
 use Illuminate\Support\Facades\Route;
 
 
-
+Route::get('/', function () {
+    return redirect('/dte');
+});
 Route::get('/dte', [DteController::class, 'form']);
 Route::post('/dte/generar', [DteController::class, 'generar']);
