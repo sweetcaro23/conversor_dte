318b06428c366d444d403d67e808069772b70b4a
sweetcaro23 <carolinafloresm.analista@gmail.com>
2026-02-27 17:15:26 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,11 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+    public function boot(): void
+    {
+    URL::forceScheme('https');
+    }
+
     /**
      * Bootstrap any application services.
      */
